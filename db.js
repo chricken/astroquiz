77dd3448b13d77a53bafbe86466edbd8518cdaed
C. Heisch <a@b.c>
2025-05-30 13:14:38 +0200

    initilizationierung

---
+++ db.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import questions from './prefill/questions.json' with {type: 'json'};
+import views from './prefill/views.json' with {type: 'json'};
+
+const db = {
+    init() {
+
+    },
+    prefill() {
+
+    }
+}
+
+export default db;
---
+++ notes.md
@@ -0,0 +1,2 @@
+Der Client lädt initial eine Liste aller IDs mit der Schwierigkeit aus der Datenbank. 
+Dies soll aus einem View, der auch in preload abgelegt werden soll, erledigt werden.
